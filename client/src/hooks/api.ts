a1a5b5e80208773c740681acf6ad1213f2c8da04
m3rashid <m3rashid.hussain@gmail.com>
2023-05-31 19:10:39 +0530

    feat(permission): init

--- client/src/hooks/api.ts
+++ client/src/hooks/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'http://64.227.138.149',
   headers: {
     Authorization: `Bearer ${window.localStorage.getItem('token')}`,
   },
