fe3a4819a0fc3ff024a9fe76cab13fa757eb288e
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2023-06-01 20:48:36 +0530

    Update api.ts

--- client/src/hooks/api.ts
+++ client/src/hooks/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://64.227.138.149',
-  baseURL: 'https://shiblirca.m3rashid.in',
+  // baseURL: 'https://shiblirca.m3rashid.in',
+  baseURL: 'http://rcaback.exatorial.com',
   headers: {
     Authorization: `Bearer ${window.localStorage.getItem('token')}`,
     'Access-Control-Allow-Origin': '*',
