ecccb76182e60ec25f5c6c5d30ad29490ca21991
m3rashid <m3rashid.hussain@gmail.com>
2023-07-06 02:34:36 +0530

    feat(models): update

--- client/src/hooks/api.ts
+++ client/src/hooks/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const getToken = () => window.localStorage.getItem('token');
 
-export const baseURL = 'http://localhost:4000'; // local
-// export const baseURL = 'https://rcaback.exatorial.com', // prod
+// export const baseURL = 'http://localhost:4000'; // local
+export const baseURL = 'https://rcaback.exatorial.com'; // prod
 
 const instance = axios.create({
   baseURL,
